184e5687522186e8980890fa410bdb9c898e64c3
Aly-El-Badry <ali.el.badry.747@gmail.com>
2025-08-27 18:47:40 +0300

    initialize mlflow

--- run_pipeline.py
+++ run_pipeline.py
@@ -2,5 +2,5 @@ from pipelines.dataProcesssingPipeline import data_preprocessing_pipeline
 from pipelines.trainingPipeline import training_pipeline
 
 if __name__ == "__main__":
-    # data_preprocessing_pipeline()
+    data_preprocessing_pipeline()
     training_pipeline()
